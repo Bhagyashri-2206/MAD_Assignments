cb94d9629e806b3e88f1d4fbb5db48070b161dd6
Bhagyashri Patil <patilbhagyashri2206@gmail.com>
2026-04-07 03:08:51 +0530

    Add files via upload

---
+++ AssignmentNo_3/Frame_Layout/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.framelayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;   // ✅ REQUIRED IMPORT
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
+    }
+}
