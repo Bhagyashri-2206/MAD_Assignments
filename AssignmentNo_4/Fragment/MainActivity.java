328c9b244a3d9cfd005eefc6bc28536dec6b5247
Bhagyashri Patil <patilbhagyashri2206@gmail.com>
2026-04-07 20:25:17 +0530

    Add files via upload

---
+++ AssignmentNo_4/Fragment/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FirstFragment fragment = new FirstFragment();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.fragment_container, fragment);
+        ft.commit();
+    }
+}
